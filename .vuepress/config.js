48e4b7d350246121bb17ecffc082a71e4cf4ebf6
yangyang <yangyang@192.168.0.100>
2021-10-03 18:17:43 +0800

    重新写master分支内容

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,46 +1,21 @@
 module.exports = {
-  title: 'XXX的博客',
-  description: 'XXX的博客',
-  dest: './dist',
-  port: '8080',
-  head: [
-      ['link', {rel: 'icon', href: '/logo.jpg'}]
-  ],
-  markdown: {
-      lineNumbers: true
-  },
-  themeConfig: {
-      nav: [{
-          text: '指南', link: '/guide/'
-      }],
-      sidebar: {'/guide/':[
-          {
-                title:'新手指南',
-                collapsable: true,
-                children:[
-                  '/guide/notes/one',
-                ]
-              },
-              {
-                title:'java',
-                collapsable: true,
-                children:[
-                  '/guide/notes/two',
-                ]
-              }
-          ]
-      },
-      sidebarDepth: 2,
-      lastUpdated: 'Last Updated',
-      searchMaxSuggestoins: 10,
-      serviceWorker: {
-          updatePopup: {
-              message: "有新的内容.",
-              buttonText: '更新'
-          }
-      },
-      editLinks: true,
-      editLinkText: '在 GitHub 上编辑此页 ！'
-  }
-}
+  lang: 'zh-CN',
+  title: '你好， VuePress ！',
+  description: '这是我的第一个 VuePress 站点',
 
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+    nav: [
+      {
+        text: 'HTML/CSS',
+        ariaLabel: 'Language Menu',
+        items: [
+          { text: 'html', link: '/html' },
+          { text: 'css', link: '/css' }
+        ]
+      },
+      { text: 'JavaScript', link: 'JavaScript' }
+    ],
+    
+  },
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ---
 home: true
-heroImage: /logo.jpg
+heroImage: ./cat.jpg
 actionText: 快速上手 →
 actionLink: /zh/guide/
 features:
