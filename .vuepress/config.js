03978b39a6d96b8e06a6d00e0680e1a6520d0711
yangyang <yangyang@192.168.0.100>
2021-10-03 18:31:41 +0800

    重新写master分支内容

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   lang: 'zh-CN',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
-
+  base: '/blog',
   themeConfig: {
     logo: 'https://vuejs.org/images/logo.png',
     nav: [
